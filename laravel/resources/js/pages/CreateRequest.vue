fa339e8dbdae3bf8361851402cd102d5687323b6
Patrick Silva <patricksssilva@yahoo.com>
2024-12-03 11:05:40 +0800

    Continued layout for Creating Request

--- laravel/resources/js/pages/CreateRequest.vue
+++ laravel/resources/js/pages/CreateRequest.vue
@@ -1,28 +1,64 @@
 <template>
-    <template v-if="formStep===1">
-        <div><label>Category</label><div>
-            <select id="item_category"
+    <template v-if="formStep===1"  >
+        <section class="bg-gray-100 min-h-dvh flex w-full   box-border justify-center items-center">
+
+        <div   class="bg-[#dfa674] rounded-2xl  max-w-3xl flex p-5 " >
+            <div class="md:w-1/2 px-8">
+   
+    
+    <div><label>Category</label></div><div>
+            <select class="p-1 mt-1 rounded-xl border  w-auto" id="item_category"
       v-model="form.item_category"
     >
-      <option v-for="category in categories"
+      <option class='text-wrap' v-for="category in categories"
         :key="category.id"
         :value="category.id"
       >
         {{ category.type }} 
       </option>
-</select></div></div>
-        <div><label>Title</label><div><input type='text' id='title' v-model="form.title" /></div></div>
-        <div><label>Description</label><div><input type='text' id='description' v-model="form.description" /></div></div>
-        <div><label>Date Lost</label><div><datepicker id='datelost' v-model="form.datelost"></datepicker></div></div>
+</select></div>
+        <div class="w-1/2 px-1 ">
+            <div class="mt-3"><label>Title</label></div>
 
-        <button @click='goForward'>Next</button>
+            <div >
+                <input class="p-1 mt-2 rounded-xl border h-auto w-auto" type='text' id='title' v-model="form.title" />
 
+            
+            </div>
+
+        </div>
+        <div class="w-1/2 px-1 ">
+
+    <div  class="mt-3"><label>Date Lost</label></div><div><datepicker class="w-auto" id='datelost' v-model="form.datelost"></datepicker></div>
+        </div>
+
+        <div class="md:w-2/2 px-1">
+
+<div  class="mt-3"><label>Description</label></div><div >
+        
+        
+        <textarea rows="4" class="p-1 mt-1 rounded-xl border h-full w-auto" id='description' v-model="form.description"></textarea></div>
+
+    </div>
+
+        <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300"  @click='goForward'>Next</button>
+
+    </div>
+
+
+    </div>
+
+    </section>
     </template>
     <template v-else-if="formStep===2" >
-            <div><label>Where was it Last seen? (optional)</label>
+        <section class="bg-gray-100  min-h-dvh w-auto flex box-border justify-center items-center">
+
+<div   class="bg-[#dfa674] rounded-2xl flex max-w-3xl p-5 " >    
+    <div class="md:w-1/2 px-8">    
+            <div><label>Where was it Last seen? (optional)</label></div>
         <div>
 
-            <select id="item_location"
+            <select id="item_location"  class="p-1 mt-2 rounded-xl border h-auto w-auto"  
       v-model="form.item_location"
     >
       <option v-for="location in locations"
@@ -35,42 +71,46 @@
 
         </div>
     
-    
-        </div>
-        <div><label>Shape (optional)</label><div><input type='text' id="shape" v-model="form.shape" /></div></div>
-        <div><label>Color (optional)</label><div><input type='text' id="color" v-model="form.color" /></div></div>
-        <div><label>Length (optional)</label><div><input type='text' id="length" v-model="form.length" /></div></div>
-        <div><label>Width (optional)</label><div><input type='text' id="width" v-model="form.width" /></div></div>
+        <div class="mt-3" ><label>Shape (optional)</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto"  id="shape" v-model="form.shape" /></div>
+        <div class="mt-3" ><label>Color (optional)</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto"  id="color" v-model="form.color" /></div>
+        <div class="mt-3"><label>Length (optional)</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto"  id="length" v-model="form.length" /></div>
+        <div class="mt-3"><label>Width (optional)</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto"  id="width" v-model="form.width" /></div>
 
-        <button @click='goBack'>Previous</button>
-        <button @click='goForward'>Next</button>
-
+        <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goBack'>Previous</button>
+        <button class="mx-1 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goForward'>Next</button>
+</div>
+</div>
+</section>
 
     </template>
     <template v-else-if="formStep===3">
+        <section class="bg-gray-100 min-h-dvh flex box-border justify-center items-center">
+
+<div   class="bg-[#dfa674] rounded-2xl flex max-w-3xl p-5 " >    
+    <div class="md:w-1/2 px-8"> 
         <div><label>Do you have a previous picture for reference purposes? (Optional)</label>
             
             <div>
 
-                <input type="text" id="picture" v-model="form.picture" />
+                <input type="text" id="picture" class="p-1 mt-2 rounded-xl border h-auto w-auto"   v-model="form.picture" />
 
                 <input type="file" @change="handleFileUpload" id="fileUpload" />
 
 
             </div>
         </div>
-        <div><label>Do you have any further details or comments on the request?</label><input type=text v-model="form.otherdetails" /></div>
+        <div><label>Do you have any further details or comments on the request?</label><input type=text class="p-1 mt-2 rounded-xl border h-auto w-auto"   v-model="form.otherdetails" /></div>
         
-        <button @click='goBack'>Previous</button>
-        <button @click='goForward'>Next</button>
+        <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300"  @click='goBack'>Previous</button>
+        <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300"  @click='goForward'>Next</button>
 
-
+</div></div></section>
 
     </template>
     <template v-else>
            dsadfd 
-        <button @click='goBack'>Previous</button>
-        <button @click='submitForm'>Submit</button>
+        <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300"  @click='goBack'>Previous</button>
+        <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300"  @click='submitForm'>Submit</button>
 
 
         
