b38e6ed47f5e90cb883154689afb5938663c1601
Patrick Silva <patricksssilva@yahoo.com>
2024-12-04 10:06:49 +0800

    Continued layout of Registration

--- laravel/resources/js/pages/Register.vue
+++ laravel/resources/js/pages/Register.vue
@@ -6,9 +6,9 @@
 <div   class="bg-[#dfa674] rounded-2xl  max-w-3xl flex p-5 " >
     <div class="md:w-1/2 px-8">
         
-        <div><label>Username</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="username" id="username" /></div><span id='userverification' style="color:red;"></span></div>
-        <div><label>Password</label><div><input type='password' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="password" id="pass" /></div></div>
-        <div><label>Confirm Password</label><div><input type='password' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="confirmpass" id="confirmpass" /></div></div>
+        <div class="mt-3" ><label>Username</label><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="username" id="username" /><span id='userverification' style="color:red;"></span></div>
+        <div class="mt-3" ><label>Password</label><input type='password' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="password" id="pass" /></div>
+        <div class="mt-3" ><label>Confirm Password</label><input type='password' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="confirmpass" id="confirmpass" /></div>
 
         <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goForward'>Next</button>
 </div>
@@ -21,12 +21,13 @@
 <div   class="bg-[#dfa674] rounded-2xl  max-w-3xl flex p-5 " >
     <div class="md:w-1/2 px-8">
 
-        <div><label>First Name</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="firstname" id="firstname" /></div></div>
-        <div><label>Middle Name</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="middlename" id="middlename" /></div></div>
+        <div class="mt-3" ><label>First Name</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="firstname" id="firstname" /></div>
+        <div class="mt-3" ><label>Middle Name</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="middlename" id="middlename" /></div>
  
-        <div><label>Last Name</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="lastname" id="lastname" /></div></div>
-        <div><label>Age</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="age" id="age" /></div></div>
-        <div><label>Sex</label><div>
+        <div class="mt-3" ><label>Last Name</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="lastname" id="lastname" /></div>
+        <div class="mt-3" ><label>Age</label></div><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="age" id="age" /></div>
+        <div class="mt-3" ><label>Sex</label></div>
+        <div >
             
             
             <select v-model="sex" id="sex"   class="p-1 mt-2 rounded-xl border h-auto w-auto" >
@@ -34,8 +35,8 @@
                 <option value="F">Female</option>
                 <option value="NB">Non-Binary</option>
             </select>
-        </div></div>
-        <div><label>Civil Status</label><div>
+        </div>
+        <div  class="mt-3"><label>Civil Status</label>
             
             
             
@@ -52,11 +53,12 @@
         
         
         
-        </div></div>
+        </div>
 
+        <div  class="mt-3">
         <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goBack'>Previous</button>
         <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goForward'>Next</button>
-
+    </div>
 </div>
 </div>
 </section>
@@ -68,11 +70,11 @@
 <div   class="bg-[#dfa674] rounded-2xl  max-w-3xl flex p-5 " >
     <div class="md:w-1/2 px-8">
 
-        <div><label>Unit</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="unit" id="unit" /></div></div>
-        <div><label>Street Address</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="street" id="street" /></div></div>
-        <div><label>City</label><div><input type='text' v-model="city" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="city" /></div></div>
-        <div><label>Province</label><div><input type='text' v-model="province" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="province" /></div></div>
-        <div><label>Country</label>
+        <div class="mt-3" ><label>Unit</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="unit" id="unit" /></div></div>
+        <div class="mt-3" ><label>Street Address</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="street" id="street" /></div></div>
+        <div class="mt-3" ><label>City</label><div><input type='text' v-model="city" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="city" /></div></div>
+        <div class="mt-3" ><label>Province</label><div><input type='text' v-model="province" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="province" /></div></div>
+        <div class="mt-3" ><label>Country</label>
         <div>
 
             <select id="country"   class="p-1 mt-2 rounded-xl border h-auto w-auto" 
@@ -87,7 +89,7 @@
 </select>
 
         </div></div>
-        <div><label>Zip Code</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="zipcode" id="zipcode" /></div></div>
+        <div class="mt-3" ><label>Zip Code</label><div><input type='text' class="p-1 mt-2 rounded-xl border h-auto w-auto" v-model="zipcode" id="zipcode" /></div></div>
         
         <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goBack'>Previous</button>
         <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goForward'>Next</button>
@@ -103,15 +105,15 @@
 
 <div   class="bg-[#dfa674] rounded-2xl  max-w-3xl flex p-5 " >
     <div class="md:w-1/2 px-8">       
-        <div><label>Home Phone</label>
-            <div><input type='text' v-model="landline" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="landline" /></div>
+        <div class="mt-3" ><label>Home Phone</label>
+            <input type='text' v-model="landline" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="landline" />
         </div>
-        <div><label>Mobile Phone</label>
-            <div><input type='text' v-model="mobile" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="mobile" /></div>
+        <div class="mt-3" ><label>Mobile Phone</label>
+            <input type='text' v-model="mobile" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="mobile" />
         </div>
 
-        <div><label>Email</label>
-            <div><input type='text' v-model="email" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="email" /></div>
+        <div class="mt-3" ><label>Email</label>
+            <input type='text' v-model="email" class="p-1 mt-2 rounded-xl border h-auto w-auto" id="email" />
         </div>
         
         <button class="mx-2 mt-2 hover:border register text-white bg-[#002D74] hover:border-gray-400 rounded-xl py-2 px-5 hover:scale-110 hover:bg-[#002c7424] font-semibold duration-300" @click='goBack'>Previous</button>
